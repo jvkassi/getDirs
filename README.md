391d7458b0a7f28c3ce5644bfe62b54022cbda35
Andrew <titon1235@gmail.com>
2014-12-05 12:31:47 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-getDirs
-=======
+#getdirs
+==================
+###To install :
+```bash
+npm install getdirs
+```
 
-NodeJS modules to obtain directory structure
+###To use
+```js
+var getdirs=require('getdirs');
+
+getdirs.flat(path.resolve("./"),{},function(err,out)
+{
+    if (!err)
+        console.log(out);
+   //[ 'Have Better', 'I Should' ]
+});
+
+getdirs.nested(path.resolve("./"),{},function(err,out)
+{
+    if (!err)
+        console.log(out);
+    /*
+    { 
+        name: 'test',
+        dir:[ 
+                { name: 'Have Better', dir: [Object] },
+                { name: 'I Should', dir: [] } 
+            ] 
+    }
+     */
+});
+```
+
+##API
+
+###flat(directory,[options],callback)
+
+Retrieves a flat listing of all the folders within a directory,ie if there are nested folders, these are not listed.Flat listing is included for optimization.
+
+**directory**: String .Path to directory.
+
+**options**: Object
+
+Possible options:
+```
+    noHidden : Boolean .Whether to list hidden directories , ie those that start with '.'. Default : false , hidden folders will be shown 
+```
+
+**callback**: Function . Callback function that adheres to the format function(err,out)
+
+###nested(directory,[options],callback)
+
+Retrieves a nested listing of all the folders within a directory.The returned object will adhere to the structure 
+
+```
+{
+    name: Name of the folder
+    dir: Array of objects with the same structure as this that represent the nested folders
+}
+```
+
+**directory**: String .Path to directory.
+
+**options**: Object
+
+Possible options:
+```none
+    noHidden : Boolean .Whether to list hidden directories , ie those that start with '.'. Default : false , hidden folders will be shown 
+```
+
+**callback**: Function . Callback function that adheres to the format function(err,out)
---
+++ index.js
@@ -0,0 +1,136 @@
+var fs=require('fs');
+var path=require('path');
+
+exports.flat=function (dir,opt,cb)
+{
+    if (arguments.length==2)
+        cb=opt;
+
+    opt||(opt={});
+
+    var accumulator=[];
+    var inError=false;
+
+    fs.readdir(dir,function(err,files)
+    {
+        if (err)
+        {
+            inError=true;
+            return cb(err);
+        }
+        var counter= 0,c=files.length;
+
+        for (var i= 0;i<c;i++)
+        {
+            var inQuestion=files[i];
+
+            //Skip hidden files or ignored files if enabled
+            if (opt.noHidden&&inQuestion[0]=='.'||(opt.ignore&&opt.ignore.indexOf(inQuestion)!=-1))
+            {
+                counter++;
+                continue;
+            }
+
+            fs.stat(inQuestion,(function(theFile)
+            {
+                return function (err, ev)
+                {
+                    if (inError)
+                        return;
+                    if (err)
+                    {
+                        //Ignore subsequent stat results and prevent from calling callback again
+                        inError=true;
+                        return cb(err);
+                    }
+
+                    if (ev.isDirectory())
+                        accumulator.push(theFile);
+
+                    if (++counter==c)
+                        cb(null,accumulator);
+                }
+            })(inQuestion));
+        }
+    });
+};
+exports.nested=function(dir,opt,cb)
+{
+    if (arguments.length==2)
+        cb=opt;
+    opt||(opt={});
+
+
+    var accumulator={name:path.basename(dir),dir:[]};
+
+    var inError=false;
+    var callbacksFinished=0;
+    var callbacksAwaited=1;
+
+    function DirectoryCheckDone()
+    {
+        if (++callbacksFinished==callbacksAwaited)
+            cb(null,accumulator);
+    }
+
+    function getDirectories(dir,outputObj)
+    {
+
+        if (inError)
+            return;
+        fs.readdir(dir,function(err,files)
+        {
+            if (err)
+            {
+                inError=true;
+                return cb(err);
+            }
+            var counter= 0,c=files.length;
+
+            //If no files in directory ie filelength==0
+            if (counter==c)
+                DirectoryCheckDone();
+
+            for (var i= 0;i<c;i++)
+            {
+                var inQuestion=files[i];
+
+                //Skip hidden files or ignored files if enabled
+                if (opt.noHidden&&inQuestion[0]=='.')
+                {
+                    counter++;
+                    continue;
+                }
+
+                var fileDir=path.resolve(dir+"/"+inQuestion);
+                fs.stat(fileDir,(function(fileDir,theFile)
+                {
+                    return function (err, ev)
+                    {
+                        if (inError)
+                            return;
+                        if (err)
+                        {
+                            //Ignore subsequent stat results and prevent from calling callback again
+                            inError=true;
+                            return cb(err);
+                        }
+
+                        if (ev.isDirectory())
+                        {
+                            callbacksAwaited++;
+                            var newOutputObj={name:theFile,dir:[]};
+                            outputObj.push(newOutputObj);
+
+                            getDirectories(fileDir,newOutputObj.dir);
+                        }
+
+                        if (++counter==c)
+                            DirectoryCheckDone();
+                    }
+                })(fileDir,inQuestion));
+            }
+        });
+    }
+    getDirectories(dir,accumulator.dir);
+};

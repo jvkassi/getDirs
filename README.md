23789e42ea90b2e5d52f56334e507d7e3473bbf0
Andrew <titon1235@gmail.com>
2014-12-08 15:57:32 +0200

    Modified to support projection mapping

--- README.md
+++ README.md
@@ -45,31 +45,58 @@ Retrieves a flat listing of all the folders within a directory,ie if there are n
 Possible options:
 ```
     noHidden : Boolean .Whether to list hidden directories , ie those that start with '.'. Default : false , hidden folders will be shown 
+    filter:function .A function that adheres to function(dirname) which returns true if the directory must be included
 ```
 
 **callback**: Function . Callback function that adheres to the format function(err,out)
 
 ###nested(directory,[options],callback)
 
-Retrieves a nested listing of all the folders within a directory.The returned object will adhere to the structure 
+Retrieves a nested listing of all the folders within a directory.The *default* returned object will adhere to the structure 
 
 ```
 {
     name: Name of the folder
     dir: Object. Keys are folder names , values are objects with the same structure as this that represent the nested folders
     dirnames:Array of strings,containg the names of the immediate child folders
-    files:Array of filenames in directory.This property ONLY exists if includeFiles==true for the below options
+    filenames:Array of filenames in directory.This property ONLY exists if includeFiles==true for the below options
 }
 ```
+This can be changed by altering the **options** newFile and newFolder argument.
 
 **directory**: String .Path to directory.
 
 **options**: Object
 
 Possible options:
-```none
+``` none
     noHidden : Boolean .Whether to list hidden directories , ie those that start with '.'. Default : false , hidden folders will be shown 
     includeFiles:Boolean.Whether to add file listing to the returned object. Default false.
+    filter : function. A function that adheres to the format(dirname).Return false if you do not desire the file passed to you
+    newFile: function to execute on encountering a file in the filepath when includeFiles is true . function(parent,filename,absolutefilepath)
+    newFolder: function to execute on encountering a folder.function(parent,filename,absolutefilepath). Be aware that the first call will have an undefined parent as 
+    it will be the root folder object
 ```
 
-**callback**: Function . Callback function that adheres to the format function(err,out)
+**callback**: Function . Callback function that adheres to the format: function(err,out)
+
+
+```js
+//example 
+var path = require('path');
+var getdir=require('../');
+
+var newFolder=function(parent,name,abspath)
+{
+    var newFolder= new LocalFolder(name,abspath);
+    parent&&parent.link(newFolder);
+    return newFolder;
+};
+
+getdir.nested(path.resolve('./'),{noHidden:true},function(err,out)
+{
+    console.log(out);
+});
+
+
+```
--- index.js
+++ index.js
@@ -8,6 +8,12 @@ exports.flat=function (dir,opt,cb)
 
     opt||(opt={});
 
+    //Filter that accepts all
+    opt.filter||(opt.filter=function(file)
+    {
+        return true;
+    });
+
     var accumulator=[];
     var inError=false;
 
@@ -25,7 +31,7 @@ exports.flat=function (dir,opt,cb)
             var inQuestion=files[i];
 
             //Skip hidden files or ignored files if enabled
-            if (opt.noHidden&&inQuestion[0]=='.'||(opt.ignore&&opt.ignore.indexOf(inQuestion)!=-1))
+            if (opt.noHidden&&inQuestion[0]=='.')
             {
                 counter++;
                 continue;
@@ -44,7 +50,7 @@ exports.flat=function (dir,opt,cb)
                         return cb(err);
                     }
 
-                    if (ev.isDirectory())
+                    if (ev.isDirectory()&&opt.filter(theFile))
                         accumulator.push(theFile);
 
                     if (++counter==c)
@@ -54,13 +60,41 @@ exports.flat=function (dir,opt,cb)
         }
     });
 };
+
 exports.nested=function(dir,opt,cb)
 {
     if (arguments.length==2)
         cb=opt;
     opt||(opt={});
 
-    var accumulator={name:path.basename(dir),dir:{},dirnames:[]};
+    opt.filter||(opt.filter=function(name)
+    {
+        return true;
+    });
+    opt.newFile||(opt.newFile=function(parent,filename,path)
+    {
+        parent.filenames.push(filename);
+    });
+    opt.newFolder||(opt.newFolder=function(parent,dirname,path)
+    {
+       var newObj=
+       {
+           name:dirname,
+           dir:{},
+           dirnames:[]
+       };
+       opt.includeFiles&&(newObj.filenames=[]);
+
+       //Link ot parent
+        if (parent)
+        {
+            parent.dirnames.push(dirname);
+            parent.dir[dirname]=newObj;
+        }
+       return newObj;
+    });
+
+    var accumulator=opt.newFolder(null,path.basename(dir),dir);
 
     var inError=false;
     var callbacksFinished=0;
@@ -72,10 +106,11 @@ exports.nested=function(dir,opt,cb)
             cb(null,accumulator);
     }
 
-    function getDirectories(dir,outDirNames,outDir,outFiles)
+    function getDirectories(dir,parent)
     {
         if (inError)
             return;
+
         fs.readdir(dir,function(err,files)
         {
             if (err)
@@ -94,7 +129,7 @@ exports.nested=function(dir,opt,cb)
                 var inQuestion=files[i];
 
                 //Skip hidden files or ignored files if enabled
-                if (opt.noHidden&&inQuestion[0]=='.')
+                if (opt.noHidden&&inQuestion[0]=='.'||!opt.filter(path.join(dir,inQuestion)))
                 {
                     counter++;
                     continue;
@@ -117,13 +152,11 @@ exports.nested=function(dir,opt,cb)
                         if (ev.isDirectory())
                         {
                             callbacksAwaited++;
-                            var newOutputObj={name:theFile,dir:{},dirnames:[]};
-                            outDirNames.push(theFile);
-                            outDir[theFile]=(newOutputObj);
-                            getDirectories(fileDir,newOutputObj.dirnames,newOutputObj.dir,opt.includeFiles&&(newOutputObj.files=[]));
+                            var newBranch=opt.newFolder(parent,theFile,fileDir);
+                            getDirectories(fileDir,newBranch);
                         }
                         else if (opt.includeFiles)
-                            outFiles.push(theFile);
+                            opt.newFile(parent,theFile,fileDir);
 
                         if (++counter==c)
                             DirectoryCheckDone();
@@ -132,5 +165,5 @@ exports.nested=function(dir,opt,cb)
             }
         });
     }
-    getDirectories(dir,accumulator.dirnames,accumulator.dir,opt.includeFiles&&(accumulator.files=[]));
+    getDirectories(dir,accumulator);
 };
